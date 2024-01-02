9e90ad7edd758a1b553facb5301f5201eacb048f
Xoria <alexander.kizimenko@mail.ru>
2024-01-02 06:05:33 +0300

    add footer

---
+++ src/frontend/src/ts/pages/Homepage.ts
@@ -0,0 +1,8 @@
+import { Footer} from "../components/Footer";
+import { Header } from "../components/Header";
+import { SubHeader } from "../components/SubHeader";
+
+
+Header();
+SubHeader();
+Footer();
