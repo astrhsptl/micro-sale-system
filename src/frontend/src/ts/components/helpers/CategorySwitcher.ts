677af58bd46d158cbd0d7dd86fe11c75e5d3d4e8
alexander.kizimenko <alexander.kizimenko@mail.ru>
2024-01-04 23:43:49 +0300

    add ...

--- src/frontend/src/ts/components/helpers/CategorySwitcher.ts
+++ src/frontend/src/ts/components/helpers/CategorySwitcher.ts
@@ -1,8 +1,57 @@
-import { render } from "@/ts/utils/render";
+import { StateManager } from "../../state/state";
+import { render } from "../../utils/render";
 
-export const CategorySwitcher = async () => {
-    render("main-homepage", `
-        
-    `)
+interface CategoryTitle{
+    id: string | number;
+    title: string;
+    current?: boolean
+}
 
+export const CategorySwitcher = async (manager: StateManager) => {
+    let categoryArray = manager.getStatePosition("categories") as Array<CategoryTitle>;
+    let content = "";
+
+    categoryArray.forEach(category => {
+        content += `<span class="switcher-category${
+            category.current == true ?
+            " switcher-category__current" :
+        ""}">${category.title}</span>`
+    });
+
+    render("container-category__switchable", content);
+
+    let mouseDown = false;
+    let startX, scrollLeft;
+    const slider = document.querySelector('#container-category__switchable');
+    const categoryList = Array.from(document.getElementsByClassName("switcher-category"));
+
+    slider?.addEventListener('mousemove', (e: Event) => {
+        e.preventDefault();
+        if(!mouseDown) { return; }
+        const x = e.pageX - slider.offsetLeft;
+        const scroll = x - startX;
+        slider!.scrollLeft = scrollLeft - scroll;
+    });
+    slider?.addEventListener('mousedown', (e: Event) => {
+        mouseDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+    slider?.addEventListener('mouseup', () => {mouseDown = false});
+    slider?.addEventListener('mouseleave', () => {mouseDown = false});
+    slider?.addEventListener('wheel', (e: WheelEventInit) => {
+        if (e.deltaY > 0) {
+            slider.scrollLeft += 100;
+        } else {
+            slider.scrollLeft -= 100;
+        }
+    });
+
+    categoryList.forEach(c => {
+        c.addEventListener("click", (e: Event) => {
+            const target = e.target;
+            console.log(e);
+            
+        })
+    });
 };
--- src/frontend/src/ts/pages/Homepage.ts
+++ src/frontend/src/ts/pages/Homepage.ts
@@ -1,37 +1,34 @@
 import { manager } from "..";
-import FetchController from "../api/FetchController";
+// import FetchController from "../api/FetchController";
 import { Footer } from "../components/Footer";
 import { HeaderWithSubHeader } from "../components/HeaderWithSubHeader";
+import { CategorySwitcher } from "../components/helpers/CategorySwitcher";
 import { render } from "../utils/render";
 
 
 const HomePage = async () => {
     const language = manager.getStatePosition("language");
     const dictionary = manager.getStatePosition("dictionary");
-    const fetchMaster = new FetchController("category/");
-
-    console.log(await fetchMaster.fetchList());
+    // const fetchMaster = new FetchController("category/");
+    // console.log(await fetchMaster.fetchList());
+    let categories = [
+        {id: 1, title: "C"},
+        {id: 2, title: "Cat"},
+        {id: 1, title: "C", current: true},
+    ];
     
+    manager.register("categories", categories);
+
 
     HeaderWithSubHeader(language, dictionary);
     Footer(dictionary);    
 
     render("main-homepage", `
         <h2 class="page-title" id="most-popular">${dictionary.mostPopular}</h2>
-        <div id="container-category__switchable">
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category">Category</span>
-            <span class="switcher-category switcher-category__current">Category</span>
-        </div>
-    `)
+        <div id="container-category__switchable"></div>
+    `);
 
+    await CategorySwitcher(manager);
 };
 
 HomePage();
-
