5dd6d6d22eb5f05cf3a0de09d92338dd37280ce5
Xoria <alexander.kizimenko@mail.ru>
2024-01-06 05:33:11 +0300

    add homepage adaptive and categories as preview

--- src/frontend/src/ts/utils/render.ts
+++ src/frontend/src/ts/utils/render.ts
@@ -1,3 +1,7 @@
 export function render(elementId: string, htmlElement: string): void {
     document.querySelector<HTMLElement>(`#${elementId}`)!.innerHTML = htmlElement;
 }
+
+export function insertableRender(htmlCode: string): string {
+    return htmlCode;
+}
